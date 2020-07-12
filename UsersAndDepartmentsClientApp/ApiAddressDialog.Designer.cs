1cf979e71fdc38465e948cdddce5660e85c07335
Sergey Sekretarev <svoboda.vezde@gmail.com>
2020-07-12 20:43:20 +0300

    Added ApiAddressDialog

---
+++ UsersAndDepartmentsClientApp/ApiAddressDialog.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace UsersAndDepartmentsClientApp
+{
+    partial class ApiAddressDialog
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtApiAddress = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Адрес API:";
+            // 
+            // txtApiAddress
+            // 
+            this.txtApiAddress.Location = new System.Drawing.Point(82, 6);
+            this.txtApiAddress.Name = "txtApiAddress";
+            this.txtApiAddress.Size = new System.Drawing.Size(202, 23);
+            this.txtApiAddress.TabIndex = 1;
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Location = new System.Drawing.Point(12, 35);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(272, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // ApiAddressdialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 71);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtApiAddress);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ApiAddressdialog";
+            this.Text = "Введите адрес API";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtApiAddress;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
---
+++ UsersAndDepartmentsClientApp/ApiAddressDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UsersAndDepartmentsClientApp
+{
+    public partial class ApiAddressDialog : Form
+    {
+        public ApiAddressDialog()
+        {
+            InitializeComponent();
+
+            btnOk.Click += Ok;
+        }
+
+        public Uri ApiOriginUri { get; private set; }
+
+        private void Ok(object sender, EventArgs e)
+        {
+            if (!Uri.TryCreate(txtApiAddress.Text, UriKind.Absolute, out var uri))
+            {
+                MessageBox.Show(this, "Неверный формат адреса!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            ApiOriginUri = uri;
+        }
+    }
+}
