c11af90edde42d64a47c865018c188e58ee927d7
Sergey Sekretarev <svoboda.vezde@gmail.com>
2020-07-12 21:37:26 +0300

    Added UriSource

---
+++ UsersAndDepartmentsClientApp/UriSource.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsersAndDepartmentsClientApp
+{
+    internal static class UriSource
+    {
+        /// <summary>
+        /// Returns uri to get list of departments.
+        /// </summary>
+        /// <returns>Uri.</returns>
+        public static Uri GetDepartmentsUri()
+        {
+            return new Uri("Departments");
+        }
+
+        /// <summary>
+        /// Returns uri to get list of users that belong to the department.
+        /// </summary>
+        /// <param name="departmentId">Unique identifier of the department.</param>
+        /// <returns>Uri.</returns>
+        public static Uri GetDepartmentUsersListUri(int departmentId)
+        {
+            return new Uri($"Users?departmentIds={departmentId}");
+        }
+
+        /// <summary>
+        /// Returns uri to work with specified department.
+        /// </summary>
+        /// <param name="departmentId">Unique identifier of the department.</param>
+        /// <returns>Uri.</returns>
+        public static Uri GetDepartmentUri(int departmentId)
+        {
+            return new Uri($"Department/{departmentId}");
+        }
+
+        /// <summary>
+        /// Returns uri to work with users of the specified department.
+        /// </summary>
+        /// <param name="departmentId">Unique identifier of the department.</param>
+        /// <returns>Uri.</returns>
+        public static Uri GetDepartmentUsersUri(int departmentId)
+        {
+            return new Uri($"Department/{departmentId}/Users");
+        }
+    }
+}
