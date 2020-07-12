c2477bbd046b52986da4d03c36175dd5f1a7f333
Sergey Sekretarev <svoboda.vezde@gmail.com>
2020-07-13 00:52:18 +0300

    Added MainForm

---
+++ UsersAndDepartmentsClientApp/MainForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace UsersAndDepartmentsClientApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReconnect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lstDepartments = new System.Windows.Forms.ListView();
+            this.btnAddDepartment = new System.Windows.Forms.Button();
+            this.btnEditDepartment = new System.Windows.Forms.Button();
+            this.btnDeleteDepartment = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstUsers = new System.Windows.Forms.ListView();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.btnMoveUser = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnReconnect
+            // 
+            this.btnReconnect.Location = new System.Drawing.Point(12, 12);
+            this.btnReconnect.Name = "btnReconnect";
+            this.btnReconnect.Size = new System.Drawing.Size(776, 23);
+            this.btnReconnect.TabIndex = 1;
+            this.btnReconnect.Text = "Указать адрес API";
+            this.btnReconnect.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lstDepartments);
+            this.groupBox1.Controls.Add(this.btnAddDepartment);
+            this.groupBox1.Controls.Add(this.btnEditDepartment);
+            this.groupBox1.Controls.Add(this.btnDeleteDepartment);
+            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 515);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Отделы";
+            // 
+            // lstDepartments
+            // 
+            this.lstDepartments.FullRowSelect = true;
+            this.lstDepartments.HideSelection = false;
+            this.lstDepartments.Location = new System.Drawing.Point(6, 22);
+            this.lstDepartments.Name = "lstDepartments";
+            this.lstDepartments.Size = new System.Drawing.Size(339, 400);
+            this.lstDepartments.TabIndex = 4;
+            this.lstDepartments.UseCompatibleStateImageBehavior = false;
+            this.lstDepartments.View = System.Windows.Forms.View.List;
+            // 
+            // btnAddDepartment
+            // 
+            this.btnAddDepartment.Location = new System.Drawing.Point(6, 428);
+            this.btnAddDepartment.Name = "btnAddDepartment";
+            this.btnAddDepartment.Size = new System.Drawing.Size(339, 23);
+            this.btnAddDepartment.TabIndex = 3;
+            this.btnAddDepartment.Text = "Добавить";
+            this.btnAddDepartment.UseVisualStyleBackColor = true;
+            // 
+            // btnEditDepartment
+            // 
+            this.btnEditDepartment.Location = new System.Drawing.Point(6, 457);
+            this.btnEditDepartment.Name = "btnEditDepartment";
+            this.btnEditDepartment.Size = new System.Drawing.Size(339, 23);
+            this.btnEditDepartment.TabIndex = 2;
+            this.btnEditDepartment.Text = "Редактировать";
+            this.btnEditDepartment.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteDepartment
+            // 
+            this.btnDeleteDepartment.Location = new System.Drawing.Point(6, 486);
+            this.btnDeleteDepartment.Name = "btnDeleteDepartment";
+            this.btnDeleteDepartment.Size = new System.Drawing.Size(339, 23);
+            this.btnDeleteDepartment.TabIndex = 1;
+            this.btnDeleteDepartment.Text = "Удалить";
+            this.btnDeleteDepartment.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstUsers);
+            this.groupBox2.Controls.Add(this.btnAddUser);
+            this.groupBox2.Controls.Add(this.btnMoveUser);
+            this.groupBox2.Location = new System.Drawing.Point(369, 41);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(419, 515);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Пользователи";
+            // 
+            // lstUsers
+            // 
+            this.lstUsers.FullRowSelect = true;
+            this.lstUsers.HideSelection = false;
+            this.lstUsers.Location = new System.Drawing.Point(6, 22);
+            this.lstUsers.Name = "lstUsers";
+            this.lstUsers.Size = new System.Drawing.Size(407, 400);
+            this.lstUsers.TabIndex = 4;
+            this.lstUsers.UseCompatibleStateImageBehavior = false;
+            this.lstUsers.View = System.Windows.Forms.View.List;
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(6, 428);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(407, 23);
+            this.btnAddUser.TabIndex = 2;
+            this.btnAddUser.Text = "Добавить";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            // 
+            // btnMoveUser
+            // 
+            this.btnMoveUser.Location = new System.Drawing.Point(6, 457);
+            this.btnMoveUser.Name = "btnMoveUser";
+            this.btnMoveUser.Size = new System.Drawing.Size(407, 23);
+            this.btnMoveUser.TabIndex = 1;
+            this.btnMoveUser.Text = "Переместить";
+            this.btnMoveUser.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 568);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnReconnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Пользователи и Отделы";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnReconnect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAddDepartment;
+        private System.Windows.Forms.Button btnEditDepartment;
+        private System.Windows.Forms.Button btnDeleteDepartment;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Button btnMoveUser;
+        private System.Windows.Forms.ListView lstDepartments;
+        private System.Windows.Forms.ListView lstUsers;
+    }
+}
---
+++ UsersAndDepartmentsClientApp/MainForm.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using UsersAndDepartmentsModel;
+
+namespace UsersAndDepartmentsClientApp
+{
+    public partial class MainForm : Form
+    {
+        private HttpClient _httpClient;
+        private RestClient _restClient;
+        private Dictionary<int, Department> _departments;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            btnReconnect.Click += Reconnect;
+            Shown += Reconnect;
+            btnAddDepartment.Click += AddDepartment;
+            btnDeleteDepartment.Click += DeleteDepartment;
+            btnEditDepartment.Click += EditDepartment;
+            lstDepartments.ItemSelectionChanged += ShowDepartmentUsers;
+            btnAddUser.Click += AddUser;
+            btnMoveUser.Click += MoveUser;
+        }
+
+        private async void MoveUser(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableUsers(false);
+
+                if (!TryGetDepartmentId(out int departmentId))
+                {
+                    return;
+                }
+
+                if (!TryGetUserId(out int userId))
+                {
+                    return;
+                }
+
+                if (_departments == null || _departments.Count == 0)
+                {
+                    return;
+                }
+
+                var dialog = new MoveUserDialog(_departments.Values);
+
+                if (dialog.ShowDialog(this) != DialogResult.OK)
+                {
+                    return;
+                }
+
+                var newDepartmentId = dialog.DepartmentId;
+
+                if (!newDepartmentId.HasValue)
+                {
+                    return;
+                }
+
+                if (newDepartmentId.Value == departmentId)
+                {
+                    return;
+                }
+
+                await _restClient.MoveUserAsync(userId, newDepartmentId.Value);
+
+                lstUsers.Items.RemoveByKey(userId.ToString());
+            }
+            finally
+            {
+                EnableUsers(true);
+            }
+        }
+
+        private async void AddUser(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableUsers(false);
+
+                if (!TryGetDepartmentId(out int departmentId))
+                {
+                    return;
+                }
+
+                var dialog = new UserDialog();
+
+                if (dialog.ShowDialog(this) != DialogResult.OK)
+                {
+                    return;
+                }
+
+                var user = await _restClient.CreateUserAsync(dialog.FullName, departmentId);
+
+                lstUsers.Items.Add(user.Id.ToString(), user.FullName, 0);
+            }
+            finally
+            {
+                EnableUsers(true);
+            }
+        }
+
+        private async void ShowDepartmentUsers(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            try
+            {
+                EnableUsers(false);
+
+                if (!e.IsSelected)
+                {
+                    return;
+                }
+
+                if (!int.TryParse(e.Item.Name, out int departmentId))
+                {
+                    return;
+                }
+
+                var users = await _restClient.GetDepartmentUsersAsync(departmentId);
+
+                try
+                {
+                    lstUsers.BeginUpdate();
+
+                    lstUsers.Items.Clear();
+
+                    foreach (var user in users)
+                    {
+                        lstUsers.Items.Add(user.Id.ToString(), user.FullName, 0);
+                    }
+                }
+                finally
+                {
+                    lstUsers.EndUpdate();
+                }
+            }
+            finally
+            {
+                EnableUsers(true);
+            }
+        }
+
+        private async void EditDepartment(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableDepartments(false);
+
+                if (!TryGetDepartmentId(out int departmentId))
+                {
+                    return;
+                }
+
+                var dialog = new DepartmentDialog();
+
+                if (dialog.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+
+                var department = await _restClient.EditDepartmentAsync(departmentId, dialog.Title);
+
+                lstDepartments.SelectedItems[0].Text = department.Title;
+
+                _departments[department.Id] = department;
+            }
+            finally
+            {
+                EnableDepartments(true);
+            }
+        }
+
+        private async void DeleteDepartment(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableAll(false);
+
+                if (!TryGetDepartmentId(out int departmentId))
+                {
+                    return;
+                }
+
+                await _restClient.DeleteDepartmentAsync(departmentId);
+
+                lstDepartments.Items.RemoveByKey(departmentId.ToString());
+                _departments.Remove(departmentId);
+
+                lstUsers.BeginUpdate();
+                lstUsers.Items.Clear();
+                lstUsers.EndUpdate();
+            }
+            finally
+            {
+                EnableAll(true);
+            }
+        }
+
+        private async void AddDepartment(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableDepartments(false);
+
+                var dialog = new DepartmentDialog();
+
+                if (dialog.ShowDialog() != DialogResult.OK)
+                {
+                    return;
+                }
+
+                var department = await _restClient.CreateDepartmentAsync(dialog.Title);
+
+                lstDepartments.Items.Add(department.Id.ToString(), department.Title, 0);
+                _departments.Add(department.Id, department);
+            }
+            finally
+            {
+                EnableDepartments(true);
+            }
+        }
+
+        private void Reconnect(object sender, EventArgs e)
+        {
+            var dialog = new ApiAddressDialog();
+
+            if (dialog.ShowDialog(this) != DialogResult.OK)
+            {
+                if (_httpClient == null)
+                {
+                    Close();
+                }
+
+                return;
+            }
+
+            if (_httpClient != null)
+            {
+                _httpClient.Dispose();
+            }
+
+            _httpClient = new HttpClient
+            {
+                BaseAddress = dialog.ApiOriginUri,
+            };
+
+            _restClient = new RestClient(_httpClient);
+
+            UpdateDepartments();
+        }
+
+        private async void UpdateDepartments()
+        {
+            try
+            {
+                EnableAll(false);
+
+                _departments = (await _restClient.GetDepartmentsAsync()).ToDictionary(d => d.Id);
+
+                lstDepartments.BeginUpdate();
+
+                try
+                {
+                    lstDepartments.Items.Clear();
+
+                    foreach (var department in _departments)
+                    {
+                        lstDepartments.Items.Add(department.Key.ToString(), department.Value.Title, 0);
+                    }
+                }
+                finally
+                {
+                    lstDepartments.EndUpdate();
+                }
+
+                lstUsers.BeginUpdate();
+                lstUsers.Items.Clear();
+                lstUsers.EndUpdate();
+            }
+            finally
+            {
+                EnableAll(true);
+            }
+        }
+
+        private void EnableDepartments(bool enabled)
+        {
+            lstDepartments.Enabled = enabled;
+            btnAddDepartment.Enabled = enabled;
+            btnEditDepartment.Enabled = enabled;
+            btnDeleteDepartment.Enabled = enabled;
+
+            btnReconnect.Enabled = enabled;
+        }
+
+        private void EnableUsers(bool enabled)
+        {
+            lstUsers.Enabled = enabled;
+            btnAddUser.Enabled = enabled;
+            btnMoveUser.Enabled = enabled;
+
+            btnReconnect.Enabled = enabled;
+        }
+
+        private void EnableAll(bool enabled)
+        {
+            EnableDepartments(enabled);
+            EnableUsers(enabled);
+
+            btnReconnect.Enabled = enabled;
+        }
+
+        private bool TryGetDepartmentId(out int departmentId)
+        {
+            departmentId = default;
+
+            if (lstDepartments.SelectedItems.Count == 0)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(lstDepartments.SelectedItems[0].Name, out departmentId))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool TryGetUserId(out int userId)
+        {
+            userId = default;
+
+            if (lstUsers.SelectedItems.Count == 0)
+            {
+                return false;
+            }
+
+            if (!int.TryParse(lstUsers.SelectedItems[0].Name, out userId))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
