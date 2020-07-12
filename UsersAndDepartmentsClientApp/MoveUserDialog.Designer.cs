b5f2448073cae57e94552b5acd54e416939b60a5
Sergey Sekretarev <svoboda.vezde@gmail.com>
2020-07-13 00:40:53 +0300

    Added MoveUserDialog

---
+++ UsersAndDepartmentsClientApp/MoveUserDialog.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace UsersAndDepartmentsClientApp
+{
+    partial class MoveUserDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDepartment = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbDepartment
+            // 
+            this.cmbDepartment.FormattingEnabled = true;
+            this.cmbDepartment.Location = new System.Drawing.Point(61, 6);
+            this.cmbDepartment.Name = "cmbDepartment";
+            this.cmbDepartment.Size = new System.Drawing.Size(315, 23);
+            this.cmbDepartment.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Отдел:";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(12, 35);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(171, 23);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(189, 35);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(187, 23);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Отмена";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // MoveUserDialog
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(389, 71);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbDepartment);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MoveUserDialog";
+            this.Text = "MoveUserDialog";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDepartment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ UsersAndDepartmentsClientApp/MoveUserDialog.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using UsersAndDepartmentsModel;
+
+namespace UsersAndDepartmentsClientApp
+{
+    public partial class MoveUserDialog : Form
+    {
+        public MoveUserDialog(IEnumerable<Department> departments)
+        {
+            InitializeComponent();
+
+            foreach (var department in departments)
+            {
+                cmbDepartment.Items.Add(new DepartmentWrapper(department));
+            }
+
+            btnOk.Click += Ok;
+        }
+
+        public int? DepartmentId => (cmbDepartment.SelectedItem as DepartmentWrapper)?.Department.Id;
+
+        private void Ok(object sender, EventArgs e)
+        {
+            if (cmbDepartment.SelectedItem == null)
+            {
+                return;
+            }
+
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+
+        private sealed class DepartmentWrapper
+        {
+            public DepartmentWrapper(Department department)
+            {
+                Department = department ?? throw new ArgumentNullException(nameof(department));
+            }
+
+            public Department Department { get; }
+
+            public override string ToString()
+            {
+                return Department.Title;
+            }
+        }
+    }
+}
