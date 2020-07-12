7b6464b48d3d3d83133c7f5ec8fd12a5367c097e
Sergey Sekretarev <svoboda.vezde@gmail.com>
2020-07-12 18:30:28 +0300

    Added DepartmentsController

---
+++ UsersAndDepartmentsWebApp/Controllers/DepartmentsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using UsersAndDepartmentsWebApp.Model;
+
+namespace UsersAndDepartmentsWebApp.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        private readonly StorageDbContext _db;
+
+        /// <summary>
+        /// Creates an instance of the <see cref="DepartmentsController"/>.
+        /// </summary>
+        /// <param name="db"></param>
+        public DepartmentsController(StorageDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// Creates new department.
+        /// </summary>
+        /// <param name="title">Title of the new department.</param>
+        /// <returns>Created department.</returns>
+        [HttpPost]
+        public ActionResult<Department> Create(string title)
+        {
+            var department = new Department
+            {
+                Title = title,
+            };
+
+            _db.Add(department);
+            _db.SaveChanges();
+
+            return department;
+        }
+
+        /// <summary>
+        /// Lists all departments.
+        /// </summary>
+        [HttpGet]
+        public List<Department> Get()
+        {
+            return _db.Departments.ToList();
+        }
+    }
+}
